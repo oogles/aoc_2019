1990bd0ee7986bbeef680a02abbb6a7ed0a53c15
Alex Church <alex@church.id.au>
2019-12-02 06:54:14 +1100

    Initial commit. Added base.

---
+++ solutions/_template.py
@@ -0,0 +1,22 @@
+from . import AdventOfCode
+
+
+class Day(AdventOfCode):
+    
+    day = x
+    
+    def _get_input_line_data(self, input_line):
+        
+        return input_line
+    
+    def _part1(self, input_data):
+        
+        pass
+    
+    def _part2(self, input_data):
+        
+        pass
+
+
+if __name__ == '__main__':
+    Day(test=True).solve()
