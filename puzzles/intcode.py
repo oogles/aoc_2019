ad05b20942524b07d724e6489caa78c98470ecb8
Alex Church <alex@church.id.au>
2019-12-05 18:01:49 +1100

    Solved day 5, part 1.

--- puzzles/intcode.py
+++ puzzles/intcode.py
@@ -1,6 +1,26 @@
 import operator
 
 
+def _input():
+    
+    while True:
+        value = input('Input: ')
+        
+        try:
+            value = int(value)
+            break
+        except ValueError:
+            print('Invalid input. Must be an integer.')
+            continue
+    
+    return value
+
+
+def _output(value):
+    
+    print(value)
+
+
 def run_program(program):
     
     # Initialise memory with a copy of the intcode program
@@ -8,28 +28,60 @@ def run_program(program):
     
     instruction_pointer = 0
     while instruction_pointer < len(program):
-        opcode = memory[instruction_pointer]
+        
+        instruction_descriptor = str(memory[instruction_pointer])
+        opcode = int(instruction_descriptor[-2:])
+        param_modes = instruction_descriptor[:-2]
         
         if opcode == 99:
             break
         elif opcode == 1:
             operation = operator.add
+            num_input_params = 2
+            has_output_param = True
         elif opcode == 2:
             operation = operator.mul
+            num_input_params = 2
+            has_output_param = True
+        elif opcode == 3:
+            operation = _input
+            num_input_params = 0
+            has_output_param = True
+        elif opcode == 4:
+            operation = _output
+            num_input_params = 1
+            has_output_param = False
         else:
-            raise Exception('Unrecognised opcode')
+            raise Exception(f'Unrecognised opcode ({opcode}).')
         
-        param1 = memory[instruction_pointer + 1]
-        param2 = memory[instruction_pointer + 2]
-        param3 = memory[instruction_pointer + 3]
+        inputs = []
+        for i in range(1, num_input_params + 1):
+            value = memory[instruction_pointer + i]
+            
+            try:
+                mode = param_modes[-i]
+            except IndexError:
+                mode = '0'
+            
+            if mode == '0':
+                # Positional mode, retrieve the real value from the specified
+                # memory address. A memory address cannot be negative.
+                if value < 0:
+                    raise Exception(f'Negative memory address ({value})!')
+                
+                value = memory[value]
+            
+            inputs.append(value)
         
-        input1 = memory[param1]
-        input2 = memory[param2]
+        result = operation(*inputs)
         
-        memory[param3] = operation(input1, input2)
+        if has_output_param:
+            output_pointer = memory[instruction_pointer + num_input_params + 1]
+            memory[output_pointer] = result
         
-        # Proceed to next instruction
-        instruction_pointer += 4
+        # Proceed to next instruction, skipping over the instruction descriptor
+        # and the appropriate number of parameters
+        instruction_pointer += (1 + num_input_params + (1 if has_output_param else 0))
     else:
         raise Exception('Reached end of program without seeing opcode 99')
     
